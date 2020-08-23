627da0905348d153d6b511f6ab7eb5cefa90edf8
Qiugu He <kaochi545121151@gmail.com>
2020-08-23 17:30:25 -0500

    Created task model, migrated to MySQL DB

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_no = models.CharField(max_length=32, unique=True)
+    task_desc = models.CharField(max_length=32)
