5c0175273ad44b76c8d3b65041eae03ac74c6a1f
Qiugu He <kaochi545121151@gmail.com>
2020-08-23 17:42:35 -0500

    Init urls, added view and template for showing list of tasks

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,17 @@
+import MySQLdb
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+# listing tasks
+def index(request):
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="clockifyclone", charset='utf8')
+    with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
+        cursor.execute("SELECT id,task_no,task_desc FROM taskmanager_task")
+        tasks = cursor.fetchall()
+    return render(request, 'task/index.html', {'tasks': tasks})
+
+
 from django.shortcuts import render
 
 # Create your views here.
