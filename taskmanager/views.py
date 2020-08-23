08d826ff9c0eab25d7e472eb384b48b228b04dc0
Qiugu He <kaochi545121151@gmail.com>
2020-08-23 17:50:47 -0500

    Init urls, added view and template for add new tasks

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,7 +1,6 @@
 import MySQLdb
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
 # listing tasks
 def index(request):
@@ -11,7 +10,16 @@ def index(request):
         tasks = cursor.fetchall()
     return render(request, 'task/index.html', {'tasks': tasks})
 
-
-from django.shortcuts import render
-
-# Create your views here.
+# add new tasks
+def add(request):
+    if request.method == 'GET':
+        return render(request, 'task/add.html')
+    else:
+        task_no = request.POST.get('task_no', '')
+        task_desc = request.POST.get('task_desc', '')
+        conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="clockifyclone", charset='utf8')
+        with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
+            cursor.execute("INSERT INTO taskmanager_task (task_no,task_desc) "
+                           "values (%s,%s)", [task_no, task_desc])
+            conn.commit()
+        return redirect('../')
