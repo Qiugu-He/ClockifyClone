6686b7c405c90c8c9c2bfcb772ac5e5b09184d03
Qiugu He <kaochi545121151@gmail.com>
2020-08-25 23:26:02 -0500

    Added creation date and update date fields from Task model

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Task(models.Model):
     task_no = models.CharField(max_length=32, unique=True)
-    task_desc = models.CharField(max_length=32)
+    task_desc = models.CharField(max_length=255)
+    create_date = models.DateTimeField(auto_now_add=True)  # creation time
+    update_Date = models.DateTimeField(auto_now=True)  # updated time
+
